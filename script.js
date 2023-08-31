95cb891b3b755d44e25ae8d4a6337415e57a1fdd
Camila Piñero <cmlpr9@gmail.com>
2023-08-31 16:14:31 -0300

    fix errors with operators, change ui

--- script.js
+++ script.js
@@ -34,25 +34,46 @@ equal.addEventListener("click", (event) => {
 	display.innerHTML = result;
 });
 
-document.getElementById("c").addEventListener("click", (e) => {
+function clear() {
 	display.innerHTML = "";
 	document.getElementById("result").innerText = "";
 	result = number1 = number2 = 0;
 	operator = false;
+}
+document.getElementById("c").addEventListener("click", (e) => {
+	clear();
 });
 
 document.getElementById("ce").addEventListener("click", (e) => {
 	document.getElementById("result").innerText = "";
 });
 
+buttons.forEach((button) =>
+	button.addEventListener("click", (e) => {
+		if (document.getElementById("result").innerText == "error") {
+			clear();
+			return;
+		}
+	})
+);
+
 buttonsNum.forEach((button) =>
 	button.addEventListener("click", (e) => {
-		document.getElementById("result").innerText += `${e.target.id}`;
+		if (
+			document.getElementById("result").innerHTML.includes(".") &&
+			e.target.id === "."
+		) {
+			return;
+		} else {
+			document.getElementById("result").innerText += `${e.target.id}`;
+			buttonsOpe.forEach((button) => button.removeAttribute("disabled"));
+		}
 	})
 );
 
 buttonsOpe.forEach((button) =>
 	button.addEventListener("click", (e) => {
+		e.target.setAttribute("disabled", "");
 		display.innerText += `${document.getElementById("result").innerText}${
 			e.target.id
 		}`;
@@ -61,32 +82,32 @@ buttonsOpe.forEach((button) =>
 );
 
 function executeOperation(number1, operator, number2) {
-	try {
-		switch (true) {
-			case operator === "+":
-				result = number1 + number2;
+	switch (true) {
+		case operator === "+":
+			result = number1 + number2;
+			break;
+		case operator === "-":
+			result = number1 - number2;
+			break;
+		case operator === "*":
+			result = number1 * number2;
+			break;
+		case operator === "/" && number2 === 0:
+			result = "error";
+			return result;
+		case operator === "/":
+			result = number1 / number2;
+			if (result === Infinity) {
+				result = "error";
 				break;
-			case operator === "-":
-				result = number1 - number2;
-				break;
-			case operator === "*":
-				result = number1 * number2;
-				break;
-			case operator === "/":
-				result = number1 / number2;
-				if (result === Infinity) {
-					result = "error";
-				}
-				break;
-		}
-		if (result - Math.floor(result) !== 0) result = result.toFixed(3);
-
-		if (result.toString().length > 10) result = "ov err";
-		return result;
-	} catch (error) {
-		console.log("Hubo un error");
-		return;
+			}
+			break;
 	}
+
+	if (result % 1 != 0) result = parseFloat(parseFloat(result).toFixed(3));
+
+	if (result.toString().length > 10) result = "ov err";
+	return result;
 }
 
 buttons.forEach((button) =>
@@ -98,12 +119,15 @@ buttons.forEach((button) =>
 				);
 				op = event.target.id;
 				document.getElementById("result").innerText = "";
+				console.log("1ero", number1, operator, number2, op, result);
+
 				break;
 
 			case Boolean(number1) && !result && !(event.target.id === operator):
 				number2 = parseFloat(
 					document.getElementById("result").innerText
 				);
+				console.log("2do", number1, operator, number2, op, result);
 				break;
 
 			case event.target.id === operator &&
@@ -111,22 +135,29 @@ buttons.forEach((button) =>
 				Boolean(number2):
 				result = executeOperation(number1, op, number2);
 				document.getElementById("result").innerText = result;
+				display.innerHTML = result + operator;
 				number1 = result;
 				number2 = 0;
 				op = event.target.id;
+				console.log("3ero", number1, operator, number2, op, result);
 				break;
 
 			case !number2 && Boolean(number1) && Boolean(result):
 				number2 = parseFloat(event.target.id);
 				document.getElementById("result").innerText = number2;
 				result = 0;
+				console.log("4to", number1, operator, number2, op, result);
 				break;
 
 			default:
 				break;
 		}
+		console.log("fuera", number1, operator, number2);
 	})
 );
+
+//if (result === "error") clear();
+//case document.getElementById("result").innerText =="0":
 
 document.getElementById("off").addEventListener("click", (e) => {
 	display.innerHTML = "";
@@ -141,16 +172,3 @@ document.getElementById("on").addEventListener("click", (e) => {
 		.querySelectorAll("button")
 		.forEach((button) => button.removeAttribute("disabled"));
 });
-
-document.getElementById(".").addEventListener("click", (e) => {
-	if (!document.getElementById("result").innerHTML.match(".")) {
-		console.log(e);
-	} else console.log("no");
-});
-
-/* case result === undefined || result === NaN:
-				display.innerHTML = "";
-				document.getElementById("result").innerText = "";
-				result = number1 = number2 = 0;
-				operator = false;
-				break; */
